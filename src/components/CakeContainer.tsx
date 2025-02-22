7d47adf5d0c735deb0ae85a597251cba377c35e8
Bujala Santosh Reddy <bujalasanthoshreddy@gmail.com>
2025-02-21 20:56:20 -0500

    React-Redux setup

---
+++ src/components/CakeContainer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CakeContainer = () => {
+  return (
+    <>
+      <h2>Cake Shop</h2>
+      <h4>Number of cakes</h4>
+      <button>Buy cake</button>
+    </>
+  );
+};
+
+export default CakeContainer;
