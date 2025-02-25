5501db521908f6203081b7256a5c4ae4c281d4ab
Bujala Santosh Reddy <bujalasanthoshreddy@gmail.com>
2025-02-24 20:28:36 -0500

    User Reducer configuration

--- src/redux/RootReducer.tsx
+++ src/redux/RootReducer.tsx
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import cakeReducer from "./cake/CakeReducer";
 import iceCreamReducer from "./icecream/IceCreamReducer";
+import userReducer from "./users/UserReducer";
 
 const rootReducer = combineReducers({
   cakes: cakeReducer,
   iceCreams: iceCreamReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
---
+++ src/redux/users/UserActions.tsx
@@ -0,0 +1,25 @@
+import {
+  FETCH_USERS_FAILURE,
+  FETCH_USERS_REQUEST,
+  FETCH_USERS_SUCCESS,
+} from "./UserTypes";
+
+export const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUEST,
+  };
+};
+
+export const fetchUsersSuccess = (users: any) => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users,
+  };
+};
+
+export const fetchUsersFailure = (error: any) => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error,
+  };
+};
